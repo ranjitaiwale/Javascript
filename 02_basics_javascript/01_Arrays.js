f89f1c2950ea4ef857aed5bc7fa69b6dbb55e8c2
RANJIT VITTHAL AIWALE <72188933+ranjitaiwale@users.noreply.github.com>
2023-08-28 15:22:01 +0000

    codespace conf

---
+++ 02_basics_javascript/01_Arrays.js
@@ -0,0 +1,20 @@
+// array
+const myArr = [1,2,3,4]
+console.log(myArr)
+// arrays of javascript are resizable
+myArr.push(6,8,9)
+console.log(myArr)
+
+myArr.pop()
+console.log(myArr)
+
+myArr.unshift(9)
+console.log(myArr)
+myArr.shift(10)
+console.log(myArr)
+
+console.log(myArr.includes(8)) // this prints true
+console.log(myArr.includes(0)) // this will print false
+
+console.log(myArr.indexOf(8))
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Javascript
-In this repository I will be adding all the codes that I am doing while learning this JavaScript 
